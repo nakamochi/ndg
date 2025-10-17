8204cda622186ab36823501e48038b2f149c5996
Kristaps Kaupe <kristaps@blogiem.lv>
2025-10-17 20:46:59 +0300

    CI: Add ShellCheck for shell scripts

--- tools/fmt-check.sh
+++ tools/fmt-check.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 zig fmt --check .
-C_FILES=$(find ./src -type f -name '*.c' ! -name 'lv_font*')
-clang-format -style=file -dry-run -Werror $C_FILES
+readarray -t C_FILES <<< "$(find ./src -type f -name '*.c' ! -name 'lv_font*')"
+clang-format -style=file -dry-run -Werror "${C_FILES[@]}"
