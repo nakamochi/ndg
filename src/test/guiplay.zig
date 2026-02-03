01f7525a0106f35c1104c68ae503b2324c4bee18
Kristaps Kaupe <kristaps@blogiem.lv>
2026-02-03 11:30:38 +0200

    Show applied vs pulled sysupdates HEAD and highlight mismatch

--- src/test/guiplay.zig
+++ src/test/guiplay.zig
@@ -218,7 +218,13 @@ fn commWriteThread(gpa: std.mem.Allocator, w: anytype) !void {
             const sett: comm.Message.Settings = .{
                 .slock_enabled = state.slock_pincode != null,
                 .hostname = state.nodename.val(),
-                .sysupdates = .{ .url = "", .channel = .edge, .head = "" },
+                .sysupdates = .{
+                    .url = "",
+                    .channel = .edge,
+                    .head_pulled = "",
+                    .head_applied = "",
+                    .head_mismatch = false,
+                },
             };
             comm.write(gpa, w, .{ .settings = sett }) catch |err| {
                 logger.err("{}", .{err});
